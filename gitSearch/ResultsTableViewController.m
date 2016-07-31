53eef58a06ff6ed6a54296ff78edbcfcbd18843a
Alexander Camacho Gámez <alexandercg7@gmail.com>
2016-07-31 15:32:14 -0700

    Creation of controller for tableview results

---
+++ gitSearch/ResultsTableViewController.m
@@ -0,0 +1,13 @@
+//
+//  ResultsTableViewController.m
+//  gitSearch
+//
+//  Created by Alexander Camacho Gámez on 31/07/16.
+//  Copyright © 2016 Alexander Camacho Gámez. All rights reserved.
+//
+
+#import "ResultsTableViewController.h"
+
+@implementation ResultsTableViewController
+
+@end
