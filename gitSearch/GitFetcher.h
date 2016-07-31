987b44eacb227031093a23c6f1c8bb5c43d4204e
Alexander Camacho Gámez <alexandercg7@gmail.com>
2016-07-31 14:05:58 -0700

    Added class GitFetcher

---
+++ gitSearch/GitFetcher.h
@@ -0,0 +1,13 @@
+//
+//  GitFetcher.h
+//  gitSearch
+//
+//  Created by Alexander Camacho Gámez on 31/07/16.
+//  Copyright © 2016 Alexander Camacho Gámez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GitFetcher : NSObject
+
+@end
---
+++ gitSearch/GitFetcher.m
@@ -0,0 +1,13 @@
+//
+//  GitFetcher.m
+//  gitSearch
+//
+//  Created by Alexander Camacho Gámez on 31/07/16.
+//  Copyright © 2016 Alexander Camacho Gámez. All rights reserved.
+//
+
+#import "GitFetcher.h"
+
+@implementation GitFetcher
+
+@end
