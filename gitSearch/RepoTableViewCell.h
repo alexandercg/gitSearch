bddcabee640720fbfae03c20291fb8da2217a180
Alexander Camacho Gámez <alexandercg7@gmail.com>
2016-07-31 15:36:45 -0700

    Creation of custom uitablecell for results

---
+++ gitSearch/RepoTableViewCell.h
@@ -0,0 +1,18 @@
+//
+//  RepoTableViewCell.h
+//  gitSearch
+//
+//  Created by Alexander Camacho Gámez on 31/07/16.
+//  Copyright © 2016 Alexander Camacho Gámez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RepoTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *imgView;
+@property (weak, nonatomic) IBOutlet UILabel *lblName;
+@property (weak, nonatomic) IBOutlet UILabel *lblStars;
+@property (weak, nonatomic) IBOutlet UILabel *lblIssues;
+
+@end
---
+++ gitSearch/RepoTableViewCell.m
@@ -0,0 +1,15 @@
+//
+//  RepoTableViewCell.m
+//  gitSearch
+//
+//  Created by Alexander Camacho Gámez on 31/07/16.
+//  Copyright © 2016 Alexander Camacho Gámez. All rights reserved.
+//
+
+#import "RepoTableViewCell.h"
+
+@implementation RepoTableViewCell
+
+@synthesize imgView, lblName, lblStars, lblIssues;
+
+@end
