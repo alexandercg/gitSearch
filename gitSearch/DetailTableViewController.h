6e218f6b64d18bb949898a7d4698ae2dcbbe9f1b
Alexander Camacho Gámez <alexandercg7@gmail.com>
2016-07-31 15:41:05 -0700

    Creation of detail tableviewcontroller

---
+++ gitSearch/DetailTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailTableViewController.h
+//  gitSearch
+//
+//  Created by Alexander Camacho Gámez on 31/07/16.
+//  Copyright © 2016 Alexander Camacho Gámez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailTableViewController : UITableViewController
+
+@end
---
+++ gitSearch/DetailTableViewController.m
@@ -0,0 +1,13 @@
+//
+//  DetailTableViewController.m
+//  gitSearch
+//
+//  Created by Alexander Camacho Gámez on 31/07/16.
+//  Copyright © 2016 Alexander Camacho Gámez. All rights reserved.
+//
+
+#import "DetailTableViewController.h"
+
+@implementation DetailTableViewController
+
+@end
