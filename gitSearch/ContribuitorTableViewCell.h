f0604bcb1b278fec8b7d2df71fe7a782e4b12e66
Alexander Camacho Gámez <alexandercg7@gmail.com>
2016-07-31 21:08:14 -0700

    Creation of contributor cell

---
+++ gitSearch/ContribuitorTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  ContribuitorTableViewCell.h
+//  gitSearch
+//
+//  Created by Alexander Camacho Gámez on 31/07/16.
+//  Copyright © 2016 Alexander Camacho Gámez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContribuitorTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *lblUsername;
+@property (weak, nonatomic) IBOutlet UILabel *lblCommits;
+
+@end
---
+++ gitSearch/ContribuitorTableViewCell.m
@@ -0,0 +1,15 @@
+//
+//  ContribuitorTableViewCell.m
+//  gitSearch
+//
+//  Created by Alexander Camacho Gámez on 31/07/16.
+//  Copyright © 2016 Alexander Camacho Gámez. All rights reserved.
+//
+
+#import "ContribuitorTableViewCell.h"
+
+@implementation ContribuitorTableViewCell
+
+@synthesize lblUsername, lblCommits;
+
+@end
